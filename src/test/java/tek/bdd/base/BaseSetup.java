e884ffb78b5b403fd3e46f9e5ca1472828e86869
rostamrasuli <rostam.homayoon@gmail.com>
2024-08-01 20:31:52 -0400

    security test steps

--- src/test/java/tek/bdd/base/BaseSetup.java
+++ src/test/java/tek/bdd/base/BaseSetup.java
@@ -1,3 +1,4 @@
+
 package tek.bdd.base;
 
 import org.openqa.selenium.WebDriver;
@@ -6,25 +7,26 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import java.time.Duration;
 
 public class BaseSetup {
+
+    //Encapsulating driver instance
     private static WebDriver driver;
 
     public void setupBrowser() {
         driver.get("https://retail.tekschool-students.com/");
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
     }
-
 
     public void quitBrowser() {
-
-        if (driver !=null){
+        //null check before quit
+        if (driver != null) {
             driver.quit();
         }
-
     }
 
+    //Giving read-only indirect access to driver.
+    //restrict setting new driver instance
     public WebDriver getDriver() {
         return driver;
     }
-
 }
